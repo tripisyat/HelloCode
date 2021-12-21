f98938d48fa973eac9eebbd3c42d30b5b9b7b970
Lopatinsky <lopatinskyda@gmail.com>
2021-12-21 15:51:16 +0400

    add Example005_Condition_if_else

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    System.Console.WriteLine("Привет, " + username);
+}
