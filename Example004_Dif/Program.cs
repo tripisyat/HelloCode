91905781859e274305c6884467cac45edc35d3b8
Lopatinsky <lopatinskyda@gmail.com>
2021-12-21 12:38:03 +0400

    add Random

---
+++ Example004_Dif/Program.cs
@@ -0,0 +1,5 @@
+﻿double numberA = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+System.Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberB);
+System.Console.WriteLine(numberA / numberB);
