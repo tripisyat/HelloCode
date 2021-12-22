a4bc1f599c11b4a5808d964bb26fa84bc54c04e5
Dmitry Lopatinsky <Lopatinskyda@gmail.com>
2021-12-22 22:52:32 +0400

    add Example010  Method array

---
+++ Example010_Method_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11,21,31,4,15,61,17,18,19};
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; // завершаеи работу цикла
+    }
+
+    index++; // index = index + 1
+}
