bf1243df09fe688456326867cdefc1a16e42d696
Lopatinsky <lopatinskyda@gmail.com>
2021-12-27 17:00:33 +0400

    add Example014 Cycle For

---
+++ Example014_CycleFor/Program.cs
@@ -0,0 +1,13 @@
+﻿// цикл for
+
+string Method4(int count, string text)
+{
+    string result = string.Empty; //записываем в переменную пустую строку
+    for(int i = 0; i < count; i++) // в скобках сначала инициализация инкремента, потом инициализация счетчика, потом увеличение инкремента
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
