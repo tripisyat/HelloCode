7e0fc8941da8a2a0d3191c8755065e50cef5ffdc
Lopatinsky <lopatinskyda@gmail.com>
2021-12-27 18:08:31 +0400

    add Example016 add example about array sorting from min to max

---
+++ Example016_Sorting_array/Program.cs
@@ -0,0 +1,33 @@
+﻿//сортировка чисел массива от минимума к максимуму
+
+int[]array = {1, 5, 4, 3, 2, 6, 7, 1, 21};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++) // ищем минимальный элемент
+        {
+            if (array[j] < array [minPosition]) minPosition = j; // ищем минимальный элемент
+        }
+       
+        int temporary = array[i]; //обмен элементов массива местами
+        array[i] = array[minPosition]; //обмен элементов массива местами
+        array[minPosition] = temporary; //обмен элементов массива местами
+    }
+}
+
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
